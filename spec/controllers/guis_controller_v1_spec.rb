2d533c552c8345d62f6e73c6ae47479ecc1ceb54
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-10 09:51:57 -0300

    14° commit - tdd controller: consegue criar um gui e retornar status 201? RED

--- spec/controllers/guis_controller_v1_spec.rb
+++ spec/controllers/guis_controller_v1_spec.rb
@@ -20,4 +20,12 @@ RSpec.describe Api::V1::GuisController, type: :controller do
     end
   end
 
+  describe 'POST /api/v1/guis' do
+    it 'Consegue criar um gui e retornar status 201?' do
+      post :create, params: {gui: {project: 'lxde', description: 'ambiente padrão em alguns linux'}, format: :json}
+      expect(response.body).to include_json(project: 'lxde')
+      expect(response).to have_http_status(201)
+    end
+  end
+
 end
