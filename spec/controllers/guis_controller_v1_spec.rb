023ba8cf8b528d7b7c62590fc0d69a9e25780fe8
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-10 09:46:37 -0300

    12° commit - tdd controller: Consegue listar um gui especifico e retornar status 200? RED

--- spec/controllers/guis_controller_v1_spec.rb
+++ spec/controllers/guis_controller_v1_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Api::V1::GuisController, type: :controller do
     end
   end
 
+  describe 'GET /api/v1/guis/id' do
+    it 'Consegue listar um gui especifico e retornar status 200?' do
+      get :show, params: {id: @gui.id}
+      expect(response.body).to include_json(id: @gui.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
